04cefde3e426b3322c6afa1f4dfc93b160c3a94e
autopoet <2783924064@qq.com>
2026-03-07 14:31:26 +0800

    add some images

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    open: true,
+    port: 8088
+  },
   plugins: [
     vue(),
     vueDevTools(),
